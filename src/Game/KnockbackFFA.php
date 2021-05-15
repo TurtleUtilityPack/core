ba54922f467b40500ff3cfc19ca4753ee5f0cee7
BariPlayzYT <rifatbari28@gmail.com>
2021-05-15 16:57:50 +0600

    uhhhh

--- src/Game/KnockbackFFA.php
+++ src/Game/KnockbackFFA.php
@@ -18,6 +18,7 @@ class KnockbackFFA{
             Core::getInstance()->getScheduler()->scheduleDelayedTask(new countdown(0, "Spawning in...", "0 seconds", $game, $p), 20 * 1);
             Core::getInstance()->getScheduler()->scheduleDelayedTask(new countdown(5, "Spawning in...", "1 seconds", $game, $p), 20 * 2);
             Core::getInstance()->getScheduler()->scheduleDelayedTask(new countdown(4, "Spawning in...", "2 seconds", $game, $p), 20 * 3);
+            Core::getInstance()->getScheduler()->scheduleDelayedTask(new countdown(3, "Spawning in...", "3 seconds", $game, $p), 20 * 4);
             Core::getInstance()->getScheduler()->scheduleDelayedTask(new countdown(2, "Spawning in...", "4 seconds", $game, $p), 20 * 5);
             Core::getInstance()->getScheduler()->scheduleDelayedTask(new countdown(1, "Spawning in...", "5 seconds", $game, $p), 20 * 6);
             giveItems::giveKit(Modes::SUMO, $p);
