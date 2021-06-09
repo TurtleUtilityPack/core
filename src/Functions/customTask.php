57f82151731fce56e993733867221143227e98db
BariPlayzYT <rifatbari28@gmail.com>
2021-06-09 11:22:25 +0600

    customTask to do anything

---
+++ src/Functions/customTask.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Core\Functions;
+
+use pocketmine\scheduler\Task;
+
+
+class customTask extends Task
+{
+
+    public $task;
+
+    public function __construct($task){
+     $this->task = $task;
+    }
+
+    public function onRun(int $tick){
+    $this->task;
+    }
+
+
+}
