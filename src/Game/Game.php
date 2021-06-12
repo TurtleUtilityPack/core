b547913ba0c0092f298059bfbe23b8570bd1c051
BariPlayzYT <rifatbari28@gmail.com>
2021-06-12 19:33:05 +0600

    useful functions for Game obj

--- src/Game/Game.php
+++ src/Game/Game.php
@@ -3,12 +3,15 @@
 namespace Core\Game;
 
 
+use libReplay\data\Replay;
+
 class Game{
 
     public $type;
     public $mode;
     public $players = [];
-    public $replay;
+    public $id = null;
+    public ?Replay $replay = null;
     public $finished = false;
 
     public function __construct(array $players, $type, $mode){
@@ -17,6 +20,27 @@ class Game{
     $this->mode = $mode;
     $this->players = $players;
 
+    }
+
+
+    public function getPlayers(){
+    return $this->players;
+    }
+
+    public function setId($id){
+    $this->id = $id;
+    }
+
+    public function isFinished(){
+    if($this->finished){
+     return true;
+    } else {
+     return false;
+     }
+    }
+
+    public function setState($state){
+    $this->finished = $state;
     }
 
 
